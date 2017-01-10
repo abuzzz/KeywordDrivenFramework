99be19a1a3c822f784b5935c86d8916ab3fb8ab2
ajameel <ajameel@XEN-783.xavient.com>
2017-01-10 12:09:53 +0530

    elements

--- Utils/src/main/java/selenium_utls/Elements.java
+++ Utils/src/main/java/selenium_utls/Elements.java
@@ -2,54 +2,55 @@ package selenium_utls;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+//WebElement locator
 
-public class Elements extends Browsers {
+public class Elements extends BaseClass {
 
 	public static WebElement getWebElementByXpath(String xpath) {
 
-		return DRIVER.findElement(By.xpath(xpath));
+		return getDriver().findElement(By.xpath(xpath));
 
 	}
 
 	public static WebElement getWebElementById(String id) {
 
-		return DRIVER.findElement(By.id(id));
+		return getDriver().findElement(By.id(id));
 
 	}
 
 	public static WebElement getWebElementByName(String name) {
 
-		return DRIVER.findElement(By.name(name));
+		return getDriver().findElement(By.name(name));
 
 	}
 
 	public static WebElement getWebElementByLinkText(String linkText) {
 
-		return DRIVER.findElement(By.linkText(linkText));
+		return getDriver().findElement(By.linkText(linkText));
 
 	}
 
 	public static WebElement getWebElementByPartialLinkText(String partialLinkText) {
 
-		return DRIVER.findElement(By.partialLinkText(partialLinkText));
+		return getDriver().findElement(By.partialLinkText(partialLinkText));
 
 	}
 
 	public static WebElement getWebElementByClassName(String className) {
 
-		return DRIVER.findElement(By.className(className));
+		return getDriver().findElement(By.className(className));
 
 	}
 
 	public static WebElement getWebElementByCssSelector(String selector) {
 
-		return DRIVER.findElement(By.cssSelector(selector));
+		return getDriver().findElement(By.cssSelector(selector));
 
 	}
 
 	public static WebElement getWebElementByC(String tagName) {
 
-		return DRIVER.findElement(By.tagName(tagName));
+		return getDriver().findElement(By.tagName(tagName));
 
 	}
 
