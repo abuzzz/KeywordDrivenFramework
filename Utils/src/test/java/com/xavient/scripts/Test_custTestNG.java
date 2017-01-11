3c19710af341ce52e8941de16f87d8b6763adfc4
ajameel <ajameel@XEN-783.xavient.com>
2017-01-11 14:23:26 +0530

    test script

---
+++ Utils/src/test/java/com/xavient/scripts/Test_custTestNG.java
@@ -0,0 +1,24 @@
+package com.xavient.scripts;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+ 
+public class Test_custTestNG {
+    @Test
+    public void testOne() {
+        Assert.assertTrue(true);
+        System.out.println("Pass Test case - testOne");
+    }
+ 
+    @Test
+    public void testTwo() {
+        Assert.assertEquals("AutomationgTesting", "AutomationgTest");
+        System.out.println("Pass Test case - testOne");
+    }
+ 
+    @Test
+    public void testThree() {
+        Assert.assertTrue(true);
+        System.out.println("Pass Test case - testThree");
+    }
+}
