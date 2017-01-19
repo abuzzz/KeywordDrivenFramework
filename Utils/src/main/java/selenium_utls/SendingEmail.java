d82f1707272d3150945148957923b64bd86a56d4
cpanigrahi <cpanigrahi@xen-454.xavient.com>
2017-01-19 15:48:57 +0530

    Script is modified

--- Utils/src/main/java/selenium_utls/SendingEmail.java
+++ Utils/src/main/java/selenium_utls/SendingEmail.java
@@ -2,7 +2,9 @@ package selenium_utls;
 
 import java.util.Properties;
 
-import javax.mail.BodyPart;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.Message;
 import javax.mail.Multipart;
 import javax.mail.Session;
@@ -14,70 +16,132 @@ import javax.mail.internet.MimeMultipart;
 
 public class SendingEmail {
 
-	public static boolean sendMail(String userName, String passWord, String host, String port, String starttls,
-			String auth, boolean debug, String socketFactoryClass, String fallback, String[] to, String[] cc,
-			String[] bcc, String subject, String text, String err, String name) {
+	public static void execute() throws Exception
 
-		
-		Properties properties = new Properties();
-		properties.put("mail.smtp.user", userName);
-		properties.put("mail.smtp.host", host);
-		if (!"".equals(port)) {
-			properties.put("mail.smtp.port", port);
-		}
-		if (!"".equals(starttls)) {
-			properties.put("mail.smtp.starttls.enable", starttls);
-			properties.put("mail.smtp.auth", auth);
-		}
-		if (debug) {
-			properties.put("mail.smtp.debug", "true");
-		} else {
-			properties.put("mail.smtp.debug", "false");
-		}
-		if (!"".equals(port)) {
-			properties.put("mail.smtp.socketFactory.port", port);
-		}
-		if (!"".equals(socketFactoryClass)) {
-			properties.put("mail.smtp.socketFactory.class", socketFactoryClass);
-		}
-		if (!"".equals(fallback)) {
-			properties.put("mail.smtp.socketFactory.fallback", fallback);
-		}
-		try {
-			Session session = Session.getDefaultInstance(properties, null);
-			session.setDebug(debug);
-			MimeMessage msg = new MimeMessage(session);
-			msg.setText(text);
-			msg.setSubject(subject);
-			Multipart multipart = new MimeMultipart();
-			
-			
-			BodyPart messageBodyPart1 = new MimeBodyPart();
-			String body_text = "Please find the below message \n\n\nMethod Name : " + name + "\n\nReason for failing : "
-					+ err;
-			messageBodyPart1.setText(body_text);
-			multipart.addBodyPart(messageBodyPart1);
-			msg.setContent(multipart);
-			msg.setFrom(new InternetAddress(userName));
-			for (int i = 0; i < to.length; i++) {
-				msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to[i]));
-			}
-			for (int i = 0; i < cc.length; i++) {
-				msg.addRecipient(Message.RecipientType.CC, new InternetAddress(cc[i]));
-			}
-			for (int i = 0; i < bcc.length; i++) {
-				msg.addRecipient(Message.RecipientType.BCC, new InternetAddress(bcc[i]));
-			}
-			msg.saveChanges();
-			Transport transport = session.getTransport("smtp");
-			transport.connect(host, userName, passWord);
-			transport.sendMessage(msg, msg.getAllRecipients());
-			transport.close();
-			return true;
-		} catch (Exception mex) {
-			mex.printStackTrace();
-			return false;
-		}
+	{
+		String path = "E:\\Utils_Repo\\Utils\\Reports\\Report.html";
+
+		String[] to = { "shasan@xavient.com" };
+
+		String[] cc = {};
+		String[] bcc = {};
+
+		SendingEmail.sendMail("cpanigrahi@xavient.com", "xavient@55", "smtp.outlook.com", "465", "true", "true", true,
+				"javax.net.ssl.SSLSocketFactory", "false", to, cc, bcc, "testReports",
+				"Please find the reports attached", path, "emailReports");
+
 	}
 
+	public static boolean sendMail(String userName, String passWord, String host, String port, String starttls,
+			String auth, boolean debug, String socketFactoryClass, String fallback, String[] to, String[] cc,
+			String[] bcc, String subject, String text, String attachmentPath, String attachmentName) {
+
+		Properties props = new Properties();
+
+		// Properties props=System.getProperties();
+
+		props.put("mail.smtp.user", userName);
+
+		props.put("mail.smtp.host", host);
+
+		if (!"".equals(port))
+
+			props.put("mail.smtp.port", port);
+
+		if (!"".equals(starttls))
+
+			props.put("mail.smtp.starttls.enable", starttls);
+
+		props.put("mail.smtp.auth", auth);
+		// props.put("mail.smtps.auth", "true");
+
+		if (debug) {
+
+			props.put("mail.smtp.debug", "true");
+
+		} else {
+
+			props.put("mail.smtp.debug", "false");
+
+		}
+
+		if (!"".equals(port))
+
+			props.put("mail.smtp.socketFactory.port", port);
+
+		if (!"".equals(socketFactoryClass))
+
+			props.put("mail.smtp.socketFactory.class", socketFactoryClass);
+
+		if (!"".equals(fallback))
+
+			props.put("mail.smtp.socketFactory.fallback", fallback);
+
+		try
+
+		{
+
+			Session session = Session.getDefaultInstance(props, null);
+
+			session.setDebug(debug);
+
+			MimeMessage msg = new MimeMessage(session);
+
+			msg.setText(text);
+
+			msg.setSubject(subject);
+
+			Multipart multipart = new MimeMultipart();
+			MimeBodyPart messageBodyPart = new MimeBodyPart();
+			DataSource source = new FileDataSource(attachmentPath);
+			messageBodyPart.setDataHandler(new DataHandler(source));
+			messageBodyPart.setFileName(attachmentName);
+			multipart.addBodyPart(messageBodyPart);
+
+			msg.setContent(multipart);
+			msg.setFrom(new InternetAddress("shasan@xavient.com"));
+
+			for (int i = 0; i < to.length; i++) {
+
+				msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to[i]));
+
+			}
+
+			for (int i = 0; i < cc.length; i++) {
+
+				msg.addRecipient(Message.RecipientType.CC, new InternetAddress(cc[i]));
+
+			}
+
+			for (int i = 0; i < bcc.length; i++) {
+
+				msg.addRecipient(Message.RecipientType.BCC, new InternetAddress(bcc[i]));
+
+			}
+
+			msg.saveChanges();
+
+			Transport transport = session.getTransport("smtp");
+
+			transport.connect(host, userName, passWord);
+
+			transport.sendMessage(msg, msg.getAllRecipients());
+
+			transport.close();
+
+			return true;
+
+		}
+
+		catch (Exception mex)
+
+		{
+
+			mex.printStackTrace();
+
+			return false;
+
+		}
+
+	}
 }
